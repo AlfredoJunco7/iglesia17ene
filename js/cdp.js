6e396e70a5bc9b2db3ac95aad5a98249cae2668a
Alfredo Jimenez <jj026742@gmail.com>
2021-01-17 00:50:48 -0600

    pre iglesia

---
+++ js/cdp.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '20px',
+    duration:  '1500',
+    reset: true
+});
+
+sr.reveal('.tag', {
+    interval:200
+})
---
+++ js/main.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '20px',
+    duration:  '1500',
+    reset: true
+});
+
+sr.reveal('.ministerios, .nuestra', {
+    interval:200
+})
+
